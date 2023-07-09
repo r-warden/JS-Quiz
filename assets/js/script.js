5dad2603e19b6ff008a9e212d3ecf5cdf69b61ac
Richard W <richard.warden@protonmail.com>
2023-07-09 17:25:46 -0500

    created base directories and files

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var questions=[]; //will be an array of questions and answers from which we can access random question and answer sets
+question0 = {
+    question: "How do you check for a user inputs and activities in JavaScript?",
+    answerA: "eventListener",
+    answerB: "an input element",
+    answerC: "bla",
+    answerD: "bla",
+    correct: "AnswerA"
+};

0f7a148259559473b93a2b8697e20bf6bcdfea6f
Richard W <richard.warden@protonmail.com>
2023-07-12 20:51:12 -0500

    added a few questions to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,24 @@
-var questions=[]; //will be an array of questions and answers from which we can access random question and answer sets
+var questions=[ //will be an array of questions and answers from which we can access random question and answer sets
 question0 = {
     question: "How do you check for a user inputs and activities in JavaScript?",
-    answerA: "eventListener",
-    answerB: "an input element",
-    answerC: "bla",
-    answerD: "bla",
-    correct: "AnswerA"
-};
+    answerA: "Add an eventListener",
+    answerB: "Add an input element",
+    answerC: "No need, JavaScript just gets it.",
+    correct: "Add an eventListener"
+},
+question1 = {
+    question: "What does 'six' == 6 return",
+    answerA: "true",
+    answerB: "false",
+    answerC: "undefined",
+    correct: "true"
+}, 
+question2 = {
+    question: "What does 'six' === 6 return",
+    answerA: "true",
+    answerB: "false",
+    answerC: "undefined",
+    correct: "false"
+}
+];
+
